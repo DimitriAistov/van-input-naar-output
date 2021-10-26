f0985ac4b704111db777c4d7777e813792e6ae6e
Dimitri <99058899@mydavinci.nl>
2021-10-26 12:02:19 +0200

    1

---
+++ person.py
@@ -0,0 +1,18 @@
+
+name = input('Geef jouw naam: ')
+
+adress = input('Wat is uw adres ')
+
+postcode = input('Wat is uw postcode ')
+
+woonplaats = input('Wat is uw woonplaats ')
+
+
+
+
+print('----------------------------------------------------')
+print('|  Naam +      : ' + name )
+print('|  Adres +     : ' + adress )
+print('|  Postcode    : ' + postcode )
+print('|  Woonplaats  : ' + woonplaats )
+print('----------------------------------------------------')
